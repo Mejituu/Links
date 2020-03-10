cba43a824a19b5ee47543cdd53936914700e2d87
Mejituu <acgm@qq.com>
2020-03-11 00:34:23 +0800

    更新1.2.2

--- api/email-logo.php
+++ api/email-logo.php
@@ -1,1 +1,1 @@
-<?php /* 邮箱头像解API接口 */ $type = (isset($_GET['type'])) ?$_GET['type']:$_POST['type']; $email = (isset($_GET['email'])) ?$_GET['email']:$_POST['email']; $a = file_get_contents("https://api.2dph.com/email/logo?type=$type&email=$email"); echo $a; ?>
+<?php /* 邮箱头像解API接口 */ $type = (isset($_GET['type'])) ?$_GET['type']:$_POST['type']; $email = (isset($_GET['email'])) ?$_GET['email']:$_POST['email']; $a = file_get_contents("https://api.2dph.com/email/logo?type=$type&email=$email"); echo $a; /** Links by 懵仙兔兔 */ ?>
