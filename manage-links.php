369202f1509de0662b6dcca7bbf64e65d219679b
jrotty <bssf@qq.com>
2023-03-26 08:28:38 +0800

    修复typecho1.2报错问题
    1，修复没有一条友链时，typecho1.2友链设置界面报错问题（虽然报错不影响功能）
    2，调整表格间距

--- manage-links.php
+++ manage-links.php
@@ -1,5 +1,18 @@
 <?php
-include 'common.php';
+/** 初始化组件 */
+Typecho_Widget::widget('Widget_Init');
+
+/** 注册一个初始化插件 */
+Typecho_Plugin::factory('admin/common.php')->begin();
+
+Typecho_Widget::widget('Widget_Options')->to($options);
+Typecho_Widget::widget('Widget_User')->to($user);
+Typecho_Widget::widget('Widget_Security')->to($security);
+Typecho_Widget::widget('Widget_Menu')->to($menu);
+
+/** 初始化上下文 */
+$request = $options->request;
+$response = $options->response;
 include 'header.php';
 include 'menu.php';
 ?>
@@ -41,7 +54,7 @@ include 'menu.php';
                     <div class="typecho-table-wrap">
                         <table class="typecho-list-table">
                             <colgroup>
-                                <col width="10"/>
+                                <col width="15"/>
 								<col width="25%"/>
 								<col width=""/>
 								<col width="15%"/>
